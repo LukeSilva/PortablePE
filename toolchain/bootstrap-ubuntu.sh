3c2c402e11cf9afed7658d9cc463302a63e3c8a9
Wolfgange3311999 <matthew3311999@gmail.com>
2017-04-08 22:15:38 -0500

    Replace manual echo with set -x That automatically echoes commands serving the same purpose

--- toolchain/bootstrap-ubuntu.sh
+++ toolchain/bootstrap-ubuntu.sh
@@ -1,2 +1,2 @@
-echo "sudo apt install flex make bison libgmp3-dev libmpfr-dev libmpc-dev texinfo build-essential"
+set -x
 sudo apt install flex make bison libgmp3-dev libmpfr-dev libmpc-dev texinfo build-essential
