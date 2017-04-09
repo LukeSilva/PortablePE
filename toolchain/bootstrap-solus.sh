ec66bcbc2f93f95b703387ab9064220e192011df
Wolfgange3311999 <matthew3311999@gmail.com>
2017-04-08 22:19:39 -0500

    Add shebangs and make scripts executable

--- toolchain/bootstrap-solus.sh
+++ toolchain/bootstrap-solus.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 set -x
 sudo eopkg it flex-devel make bison-devel gmp-devel mpfr-devel mpc-devel texinfo -c system.devel
--- toolchain/bootstrap-ubuntu.sh
+++ toolchain/bootstrap-ubuntu.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 set -x
 sudo apt install flex make bison libgmp3-dev libmpfr-dev libmpc-dev texinfo build-essential
