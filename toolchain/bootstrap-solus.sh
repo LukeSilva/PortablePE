3508b13fde11db030c8ca164b8c44b92f31ddca0
Wolfgange3311999 <matthew3311999@gmail.com>
2017-04-08 22:12:17 -0500

    Add Solus bootstrap

---
+++ toolchain/bootstrap-solus.sh
@@ -0,0 +1,2 @@
+echo "sudo eopkg it flex-devel make bison-devel gmp-devel mpfr-devel mpc-devel texinfo -c system.devel"
+sudo eopkg it flex-devel make bison-devel gmp-devel mpfr-devel mpc-devel texinfo -c system.devel
